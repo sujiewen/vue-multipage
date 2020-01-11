7e6f09ad69366cb078f51835cbc5b085f7195f67
sjw <sujiewen@ideal-link.com>
2020-01-11 11:10:38 +0800

    cli4

--- vue-multipage/config/prod.env.js
+++ vue-multipage/config/prod.env.js
@@ -1,4 +1,49 @@
 'use strict'
-module.exports = {
+
+let buildtype = process.argv.slice(2)[0]
+console.log('buildtype='+buildtype);
+
+let obj = {
   NODE_ENV: '"production"'
 }
+
+switch (buildtype) {
+  case 'pro': //正式环境
+    process.env.srconfig = 'pro'
+    process.env.websrconfig = 'app'
+    obj.srconfig = '"pro"'
+    obj.websrconfig = '"app"'
+    break;
+  case 'proWeb': //正式环境
+    process.env.srconfig = 'pro'
+    process.env.websrconfig = 'web'
+    obj.srconfig = '"pro"'
+    obj.websrconfig = '"web"'
+    break;
+  case 'beta': //外网测试
+    process.env.srconfig = 'beta'
+    process.env.websrconfig = 'app'
+    obj.srconfig = '"beta"'
+    obj.websrconfig = '"app"'
+    break;
+  case 'betaWeb': //外网测试
+    process.env.srconfig = 'beta'
+    process.env.websrconfig = 'web'
+    obj.srconfig = '"beta"'
+    obj.websrconfig = '"web"'
+    break;
+  case 'dev': //内网测试
+    process.env.srconfig = 'dev'
+    process.env.websrconfig = 'app'
+    obj.srconfig = '"dev"'
+    obj.websrconfig = '"app"'
+    break;
+  case 'devWeb': //内网测试
+    process.env.srconfig = 'dev'
+    process.env.websrconfig = 'web'
+    obj.srconfig = '"dev"'
+    obj.websrconfig = '"web"'
+    break;
+}
+
+module.exports = obj;
